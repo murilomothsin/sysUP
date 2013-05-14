e6f6b867e4806b33f6c5fa4a1f52b4b691155ac1
Murilo Mothsin <mothsin@hotmail.com>
2013-05-14 00:38:53 -0300

    arrumado texto na pagina das tecnologias

--- technology.php
+++ technology.php
@@ -8,30 +8,28 @@
 <body>
 	<div name="home" id="home">
 		<h1>Tecnologias</h1>
-		<div class="itemTec"><img src="images/logos/HTML5.png" style="float: left; height: 100px; margin-right: 5px;"><p style="vertical-align: middle;">A linguagem HTML e XHTML vem se atualizando para o novo mundo de dispositivos portateis
-		e interatividade na web. O HTML 5 transforma um site em um verdadeiro centro de entertenimento 
+		<div class="itemTec"><img src="images/logos/HTML5.png" style="float: left; height: 100px; margin-right: 5px;"><p style="vertical-align: middle;">As linguagens HTML e XHTML vem se atualizando para o novo mundo de dispositivos portateis
+		e interatividade na web. O HTML 5 transforma um site em um verdadeiro centro de entretenimento 
 		gerando animações, gráficos e inúmeras vantagens se comparado aos seus antecessores.</p></div>
 		<div class="itemTec"><img src="images/logos/CSS3.png" style="float: left; height: 100px; margin-right: 5px;"><p style="vertical-align: middle;">O Cascading Style Sheets, ou simplesmente CSS, traz novos recursos de cantos arredondados e animações de transição de imagens que impressiona.
-		Capaz de tornar um site mais leve e mais fluido graças as novas técnologias que aboliram muitas imagens. Com CSS3 seu site terá efeitos profissionais 
-		utilizando poucos recursos de processamento e memória transformando a experiencia de navegar pelo site muito mais agradavel.</p></div>
-		<div class="itemTec"><img src="images/logos/JQuery.png" style="float: left; height: 100px; margin-right: 5px;"><p style="vertical-align: middle;">O Cascading Style Sheets, ou simplesmente CSS, traz novos recursos de cantos arredondados e animações de transição de imagens que impressiona.
-		Capaz de tornar um site mais leve e mais fluido graças as novas técnologias que aboliram muitas imagens. Com CSS3 seu site terá efeitos profissionais 
-		utilizando poucos recursos de processamento e memória transformando a experiencia de navegar pelo site muito mais agradavel.</p></div>
-		<div class="itemTec"><img src="images/logos/PHP.png" style="float: left; height: 100px; margin-right: 5px;"><p style="vertical-align: middle;">O Cascading Style Sheets, ou simplesmente CSS, traz novos recursos de cantos arredondados e animações de transição de imagens que impressiona.
-		Capaz de tornar um site mais leve e mais fluido graças as novas técnologias que aboliram muitas imagens. Com CSS3 seu site terá efeitos profissionais 
-		utilizando poucos recursos de processamento e memória transformando a experiencia de navegar pelo site muito mais agradavel.</p></div>
-		<div class="itemTec"><img src="images/logos/RubyOnRails.png" style="float: left; height: 100px; margin-right: 5px;"><p style="vertical-align: middle;">O Cascading Style Sheets, ou simplesmente CSS, traz novos recursos de cantos arredondados e animações de transição de imagens que impressiona.
-		Capaz de tornar um site mais leve e mais fluido graças as novas técnologias que aboliram muitas imagens. Com CSS3 seu site terá efeitos profissionais 
-		utilizando poucos recursos de processamento e memória transformando a experiencia de navegar pelo site muito mais agradavel.</p></div>
-		<div class="itemTec"><img src="images/logos/JAVA.png" style="float: left; height: 100px; margin-right: 5px;"><p style="vertical-align: middle;">O Cascading Style Sheets, ou simplesmente CSS, traz novos recursos de cantos arredondados e animações de transição de imagens que impressiona.
-		Capaz de tornar um site mais leve e mais fluido graças as novas técnologias que aboliram muitas imagens. Com CSS3 seu site terá efeitos profissionais 
-		utilizando poucos recursos de processamento e memória transformando a experiencia de navegar pelo site muito mais agradavel.</p></div>
-		<div class="itemTec"><img src="images/logos/MySQL.png" style="float: left; height: 100px; margin-right: 5px;"><p style="vertical-align: middle;">O Cascading Style Sheets, ou simplesmente CSS, traz novos recursos de cantos arredondados e animações de transição de imagens que impressiona.
-		Capaz de tornar um site mais leve e mais fluido graças as novas técnologias que aboliram muitas imagens. Com CSS3 seu site terá efeitos profissionais 
-		utilizando poucos recursos de processamento e memória transformando a experiencia de navegar pelo site muito mais agradavel.</p></div>
-		<div class="itemTec"><img src="images/logos/MSSQLServer.png" style="float: left; height: 100px; margin-right: 5px;"><p style="vertical-align: middle;">O Cascading Style Sheets, ou simplesmente CSS, traz novos recursos de cantos arredondados e animações de transição de imagens que impressiona.
-		Capaz de tornar um site mais leve e mais fluido graças as novas técnologias que aboliram muitas imagens. Com CSS3 seu site terá efeitos profissionais 
-		utilizando poucos recursos de processamento e memória transformando a experiencia de navegar pelo site muito mais agradavel.</p></div>
+		Capaz de tornar um site mais leve e mais fluido graças as novas tecnologias que aboliram muitas imagens. Com o CSS3 seu site terá efeitos profissionais 
+		utilizando poucos recursos de processamento e memória transformando a experiência de navegar pelo site muito mais agradavel.</p></div>
+		<div class="itemTec"><img src="images/logos/JQuery.png" style="float: left; height: 100px; margin-right: 5px;"><p style="vertical-align: middle;">Uma biblioteca JavaScript que facilita o desenvolvimento de interações com páginas web, reduzindo o tempo de desenvolvimento de aplicações web
+		com interação com o usuário. Jquery é uma solução poderosa que já domina os grandes sites e traz grandes funcionalidades como animações dentro do site
+		e resolve muitas questões de incompatibilidade entre browsers.</p></div>
+		<div class="itemTec"><img src="images/logos/PHP.png" style="float: left; height: 100px; margin-right: 5px;"><p style="vertical-align: middle;">Uma linguagem de programação web, o PHP tem influência de C/C++ e Perl, é open source e pode ser usado tanto o paradigma estruturado quanto a orientação à objetos
+		e é muito flexivel, podendo ainda ser usado algum framework como Zend Framework para agilizar no processo de desenvolvimento do site ou sistema web.</p></div>
+		<div class="itemTec"><img src="images/logos/RubyOnRails.png" style="float: left; height: 100px; margin-right: 5px;"><p style="vertical-align: middle;">É um framework escrito para Ruby que aumenta a velocidade de desenvolvimento de aplicações web que utilizam orientação para banco de dados. Esse framework 
+		utiliza tecnologias atuais e rápidas que tornam tanto o desenvolvimento quanto o resultado final excelentes. Utilizando o padrão MVC para construir a estrutura de sites tornando uma possível manutenção de codigo rápida e eficaz.</p></div>
+		<div class="itemTec"><img src="images/logos/JAVA.png" style="float: left; height: 100px; margin-right: 5px;"><p style="vertical-align: middle;">É uma linguagem que já domina o mercado empresarial de aplicativos. É ampla e possui diversas bibliotecas prontas, inclusive bibliotecas que 
+		auxiliam na troca de dados através de redes e internet. É orinetada a objetos e muito segura. Uma ótima escolha para quem precisa de um aplicativo desktop
+		como um sistema de gestão, por exemplo.</p></div>
+		<div class="itemTec"><img src="images/logos/MySQL.png" style="float: left; height: 100px; margin-right: 5px;"><p style="vertical-align: middle;">o MySQL assim como o SQL Server é um gerenciador de banco de dados, que domina o mercado, grandes empresas da atualidade como Google, NASA e bancos utilizam o MySQL.
+		O grande sucesso desse gerenciador se deve a muitos motivos entre eles estão a fácil integração com as linguagens de programação atuais, facilidade de uso, portabilidade
+		e pouca exigência de hardware do servidor.</p></div>
+		<div class="itemTec"><img src="images/logos/MSSQLServer.png" style="float: left; height: 100px; margin-right: 5px;"><p style="vertical-align: middle;">É um Sistema Gerenciador de Banco de Dados da Microsoft utilizado tanto em pequenas aplicações com alguns usuários até programas robustos com milhares de usuários 
+		conectados em todo o mundo. O SQL Server é uma poderosa ferramenta podendo gerar nativamente relatórios administrativos que auxiliam no BI da empresa, além
+		de se poder ter um rápido desenvolvimento de aplicações.</p></div>
 	</div>
 </body>
 </html>
